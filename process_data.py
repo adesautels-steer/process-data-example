8ba0fc8338943ec1156dbb5f7eda8097b3d3c970
Andrew Desautels <andrew.desautels@steergroup.com>
2023-02-24 13:03:45 -0700

    Initial commit

---
+++ process_data.py
@@ -0,0 +1,23 @@
+import json
+
+INPUT_PATH = "input_data.json"
+OUTPUT_PATH = "output_data.json"
+
+# Processing function
+def process_value(x):
+    return x * x
+
+
+# Read in raw data
+with open(INPUT_PATH, "r", encoding="utf-8") as input_data:
+    raw_values = json.load(input_data)
+
+# Create list of processed values
+processed_values = [process_value(x) for x in raw_values]
+
+# Convert to JSON
+processed_values_json = json.dumps(processed_values)
+
+# Export to file
+with open(OUTPUT_PATH, "w") as outfile:
+    outfile.write(processed_values_json)
