e7fbb8841fcadd8971b3d61a562e2fd97aac617d
Anthony Sparling <124174867+AnthonySparling@users.noreply.github.com>
2023-03-08 19:42:31 -0500

    Add a second function to decrease even values. (#1)
    * Add a second function to decrease even values
    * Give new function more descriptive name
    * Fix while loop where x wasn't updated
    
    ---------
    
    Co-authored-by: Andrew Desautels <96256990+adesautels-steer@users.noreply.github.com>

--- process_data.py
+++ process_data.py
@@ -8,9 +8,13 @@ def process_value(x):
     if (8 * x % 2) == 1:
         return 8 * x + 1
     else:
-        return 8 * x
+        return halve_till_two(x)
     
-
+# Second function for additional process
+def halve_till_two(x):
+    while x > 2:
+        x = x/2
+    return x
 
 # Read in raw data
 with open(INPUT_PATH, "r", encoding="utf-8") as input_data:
